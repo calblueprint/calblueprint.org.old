a64a5befdea6a0e35c0f653c526566bbd6761d39
Noel Moldvai <noelmoldvai@gmail.com>
2012-11-05 12:39:46 -0800

    new rails app wadup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blueprint::Application.config.secret_token = 'c5398bae6f6f1f5d90d1738a394b33f6921fae0d90a87be7559c962cd7ed184bf19805594e5c1b5c5de1e627a657ccc00c55f87771c4bb9f7c3252f388974777'
