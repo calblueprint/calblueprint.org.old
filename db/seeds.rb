6c4b746f17b3af0f027733db1193899304b9519e
Noel Moldvai <noelmoldvai@gmail.com>
2013-01-13 20:51:30 -0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Member.create(:name => "Jae Young Ryoo", :position => "President", :year => "2013", :major => "CS & MCB")
+Member.create(:name => "Noel Moldvai", :position => "VP of Technology", :year => "2013", :major => "EECS")
+Member.create(:name => "Shirley Liu", :position => "VP of Marketing and Finance", :year => "2013", :major => "Business")
+Member.create(:name => "Kevin Gong", :position => "VP of Operations", :year => "2014", :major => "CS")
+Member.create(:name => "Kenneth Gao", :position => "VP of Projects", :year => "2013", :major => "Bioengineering")
+Member.create(:name => "Omer Spillinger", :position => "Technology Chair", :year => "2013", :major => "EECS")
+Member.create(:name => "Michelle Chow", :position => "Project Leader", :year => "2013", :major => "CS & Business")
+Member.create(:name => "Brian Wong", :position => "Project Leader", :year => "2013", :major => "CS")
+Member.create(:name => "Joshua Primas", :position => "Project Leader", :year => "2013", :major => "CS")
