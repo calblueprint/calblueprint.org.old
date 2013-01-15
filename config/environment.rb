a8697202643a96ef16fa73cffe08ecea05831bc5
Noel Moldvai <noelmoldvai@gmail.com>
2013-01-14 20:33:58 -0800

    env vars

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# load environment variables
+environment_variables = File.join(Rails.root, 'config', 'environment_variables.rb')
+load(environment_variables) if File.exists?(environment_variables)
+
 # Initialize the rails application
 Blueprint::Application.initialize!
