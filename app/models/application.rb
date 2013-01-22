14b1db262fba06c95f32a293390740733e3dd9f0
Jayyr <jay.ryoo@gmail.com>
2013-01-21 23:24:48 -0800

    updated application, ready for final launch

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,7 @@
 class Application < ActiveRecord::Base
-	attr_accessible :name, :resume, :email, :phone, :year, :major, :gpa, :why_join, :development_type, :work_sample, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :referral
-	validates :name, :email, :phone, :year, :major, :gpa, :presence => true
-	validates :why_join, :work_sample, :development_type, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :presence => true
+	attr_accessible :firstname, :lastname, :resume, :email, :phone, :year, :major, :gpa, :why_join, :development_type, :work_sample, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :referral
+	validates :firstname, :lastname, :email, :phone, :year, :major, :gpa, :presence => true
+	validates :why_join, :work_sample, :development_type, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :presence => true
 	validates_attachment :resume, :presence => true,
 		:content_type => { :content_type => "application/pdf" },
 		:size => { :in => 0..1.megabytes }
