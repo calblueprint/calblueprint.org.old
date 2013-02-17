6ac0fee1a371614b39a6b56a132c39e7cb1c8f77
Noel Moldvai <noelmoldvai@gmail.com>
2013-02-16 17:16:57 -0800

    hi

--- db/migrate/20130115005154_create_applications.rb
+++ db/migrate/20130115005154_create_applications.rb
@@ -1,28 +1,28 @@
 class CreateApplications < ActiveRecord::Migration
   def change
     create_table :applications do |t|
-    	t.string :firstname
-    	t.string :lastname
-    	t.string :email
-    	t.string :phone
-    	t.string :year
-    	t.string :major
-    	t.string :gpa
-    	t.string :cs_gpa
-    	t.text :why_join
-    	t.text :work_sample
-    	t.string :development_type
-    	t.text :coursework
-    	t.text :commitments
-        t.text :rails_exp
-        t.text :mobile_exp
-    	t.string :cc_availability
-    	t.string :retreat_availability
-    	t.string :meeting_availability
-    	t.string :dinner_availability
-    	t.string :commitment_availability
-    	t.string :referral
-    	t.attachment :resume
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :phone
+      t.string :year
+      t.string :major
+      t.string :gpa
+      t.string :cs_gpa
+      t.text :why_join
+      t.text :work_sample
+      t.string :development_type
+      t.text :coursework
+      t.text :commitments
+      t.text :rails_exp
+      t.text :mobile_exp
+      t.string :cc_availability
+      t.string :retreat_availability
+      t.string :meeting_availability
+      t.string :dinner_availability
+      t.string :commitment_availability
+      t.string :referral
+      t.attachment :resume
       t.timestamps
     end
   end
