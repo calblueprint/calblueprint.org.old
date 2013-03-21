b919d87c79ecc671662d7273669d801f6a1b5a48
Noel Moldvai <noelmoldvai@gmail.com>
2013-03-21 01:18:39 -0700

    events migrations wtf

--- db/migrate/20130120060456_add_end_time_to_events.rb
+++
@@ -1,8 +0,0 @@
-class AddEndTimeToEvents < ActiveRecord::Migration
-  def change
-  	remove_column :events, :creator, :string
-  	add_column :events, :endTime, :time
-  	add_column :events, :startTime, :time
-  	change_column :events, :time, :datetime
-  end
-end
