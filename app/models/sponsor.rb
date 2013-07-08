03f3224796877000814f5e2c31a30dc6e2da38e3
callmemc <callmemc@gmail.com>
2013-07-08 01:33:12 -0700

    migrated from dropbox to s3. removed files and references associated with deprecated partners model

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,15 +1,14 @@
 class Sponsor < ActiveRecord::Base
-  attr_accessible :sponsorship_type, :link, :image
+  attr_accessible :sponsorship_type, :sponsor_link, :image
 
-  validates :sponsorship_type, :link, :presence => true
+  validates :sponsorship_type, :sponsor_link, :presence => true
   validates_attachment :image, :presence => true,
     :content_type => { :content_type => ["image/jpeg", "image/png"] },
     :size => { :in => 0..5.megabytes }
 
   has_attached_file :image,
-    :storage => :dropbox,
-    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
-    :dropbox_options => {
-      :unique_filename => true
-    }
+    :storage => :s3,
+    :s3_credentials => S3_CREDENTIALS,
+    :path => "/sponsors/:style/:id/:filename",
+    :styles => { :medium => "400px>" }
 end
