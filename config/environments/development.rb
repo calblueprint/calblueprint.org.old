f7befe75b2da5df7397ffb012c51fe3ebfbb85da
Justin Chan <hellojustinchan@gmail.com>
2013-08-26 01:01:51 -0700

    prettified

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Blueprint::Application.configure do
   config.assets.debug = true
   
   # Mailer
-  config.action_mailer.default_url_options = { :host => "calblueprint.org"}
+  config.action_mailer.default_url_options = { :host => "localhost:3000"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
