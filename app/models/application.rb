0cb10cc034550172915c5c7f973cabde6ab01253
Wonjun Jeong <jeong.wonjun@gmail.com>
2013-09-04 09:42:24 -0700

    updated application page

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,8 +1,8 @@
 class Application < ActiveRecord::Base
-	attr_accessible :firstname, :lastname, :resume, :email, :phone, :year, :major, :gpa, :why_join, :development_type, :work_sample, :coursework, :rails_exp, :mobile_exp, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :referral
+	attr_accessible :firstname, :lastname, :resume, :email, :phone, :year, :major, :gpa, :why_join, :development_type, :back_end, :graphic_design, :front_end, :work_sample, :coursework, :rails_exp, :mobile_exp, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :referral
 
 	validates :firstname, :lastname, :email, :phone, :year, :major, :gpa, :presence => true
-	validates :why_join, :work_sample, :development_type, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :presence => true
+	validates :why_join, :work_sample, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :presence => true
 	validates_attachment :resume, :presence => true,
 		:content_type => { :content_type => "application/pdf" },
 		:size => { :in => 0..1.megabytes }
