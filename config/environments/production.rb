c91c8f49be39915b9862a3da3cea8e21241b7c8f
callmemc <callmemc@gmail.com>
2013-10-17 00:16:11 -0700

    playing with assets...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Blueprint::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -64,7 +64,7 @@ Blueprint::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+
   # Mailer
   config.action_mailer.default_url_options = { :host => "calblueprint.org"}
   config.action_mailer.delivery_method = :smtp
