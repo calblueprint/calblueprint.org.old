c7cf2e1f09012a87765abbc7a175a9d1e5ef6971
Mark Miyashita <negativetwelve@gmail.com>
2013-11-16 12:49:09 -0800

    Tabs are bad, spaces are better.

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,15 +1,15 @@
 class Application < ActiveRecord::Base
-	attr_accessible :firstname, :lastname, :resume, :email, :phone, :year, :major, :gpa, :why_join, :development_type, :back_end, :graphic_design, :front_end, :work_sample, :coursework, :rails_exp, :mobile_exp, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :referral, :semester
+  attr_accessible :firstname, :lastname, :resume, :email, :phone, :year, :major, :gpa, :why_join, :development_type, :back_end, :graphic_design, :front_end, :work_sample, :coursework, :rails_exp, :mobile_exp, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :referral, :semester
 
-	validates :firstname, :lastname, :email, :phone, :year, :major, :gpa, :presence => true
-	validates :why_join, :work_sample, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :presence => true
-	validates_attachment :resume, :presence => true,
-		:content_type => { :content_type => "application/pdf" },
-		:size => { :in => 0..1.megabytes }
+  validates :firstname, :lastname, :email, :phone, :year, :major, :gpa, :presence => true
+  validates :why_join, :work_sample, :coursework, :commitments, :cc_availability, :retreat_availability, :meeting_availability, :dinner_availability, :commitment_availability, :presence => true
+  validates_attachment :resume, :presence => true,
+    :content_type => { :content_type => "application/pdf" },
+    :size => { :in => 0..1.megabytes }
 
   belongs_to :semester
 
-	has_attached_file :resume,
+  has_attached_file :resume,
     :storage => :s3,
     :s3_credentials => S3_CREDENTIALS,
     :path => "/resumes/:id/:filename"
