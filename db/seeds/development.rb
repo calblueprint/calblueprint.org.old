a153e048777885ff54e8101cedb7ecdd37ed6c49
Mark Miyashita <negativetwelve@gmail.com>
2013-11-16 12:26:54 -0800

    Fixed seeds bug...that caused faculty advisors not to be created and correctly added user: mark to the seeds file

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,6 +1,7 @@
 # create admin account
 puts 'Creating admin user.'
-User.create(:email => "admin@berk.com", :password => "password", :name => "Badass Admin", :position => "VP of Technology", :year => "2013", :major => "EECS", :is_activated => true, :is_visible => true, :is_admin => true)
+admin_user = User.create(:email => "admin@berk.com", :password => "password", :name => "Badass Admin", :position => "VP of Technology", :year => "2013", :major => "EECS", :is_activated => true, :is_visible => true, :is_admin => true)
+puts "\t Created user: #{admin_user.name}."
 
 # create other accounts
 users = [
@@ -14,21 +15,22 @@ users = [
   ["callmebeautiful@berk.com", "Michelle Chow", "Project Leader", "CS & Business"],
   ["bwong@berk.com", "Brian Wong", "Project Leader", 2013, "CS"],
   ["jp@berk.com", "Joshua Primas", "Project Leader", 2013, "CS"],
-  ["dp@berk.com", "David Patterson", "Faculty Sponsor"],
-  ["fox@berk.com", "Armando Fox", "Faculty Sponsor"],
-  ["mark@mark.com", "Mark Miyashita", "Awesome Dude", 2015, "EECS"]
+  ["dp@berk.com", "David Patterson", "Faculty Advisor"],
+  ["fox@berk.com", "Armando Fox", "Faculty Advisor"],
+  ["mark@mark.com", "Mark Miyashita", "Project Leader", 2015, "EECS"]
 ]
 
 puts 'Creating other users.'
 users.each do |u|
-  User.create(
+  new_user = User.create(
     :email => u[0],
     :password => 'password',
     :name => u[1],
     :position => u[2],
-    :year => u[3].to_s,
-    :major => u[4],
+    :year => u.length > 2 ? u[3].to_s : '',
+    :major => u.length > 3 ? u[4] : '',
     :is_activated => true,
     :is_visible => true
   )
+  puts "\t Created user: #{new_user.name}."
 end
