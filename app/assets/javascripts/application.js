4884a885ad44c0e85359e74f26083dd4550ea8ba
Mark Miyashita <negativetwelve@gmail.com>
2014-01-16 18:50:36 -0800

    Calculate the height on window onload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,8 +25,10 @@ $(function() {
     event.preventDefault();
     $("html, body").animate({ scrollTop: $('#navbar').offset().top + 1 }, 1000);
   });
+});
 
+window.onload = function() {
   $("#navbar").affix({
     offset: { top: $("#navbar").offset().top }
   });
-});
+};
