2218815568e2796fa4599760539f3694d43cb1ee
Mark Miyashita <negativetwelve@gmail.com>
2014-01-29 04:16:29 -0800

    omg it finally works

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,11 @@
 class Project < ActiveRecord::Base
   validates :client, :title, :description, :presence => true
+  has_attached_file :image,
+    :storage => :s3,
+    :s3_credentials => S3_CREDENTIALS,
+    :path => "/projects/:style/:id/:filename",
+    :styles => { :medium => "400px>" }
+
   validates_attachment :image, :presence => true,
     :content_type => { :content_type => ["image/jpeg", "image/png"] },
     :size => { :in => 0..5.megabytes }
@@ -8,12 +14,6 @@ class Project < ActiveRecord::Base
   has_many :member
 
   belongs_to :semester
-
-  has_attached_file :image,
-    :storage => :s3,
-    :s3_credentials => S3_CREDENTIALS,
-    :path => "/projects/:style/:id/:filename",
-    :styles => { :medium => "400px>" }
 
   accepts_nested_attributes_for :project_photos
 
--- app/models/project_photo.rb
+++ app/models/project_photo.rb
@@ -1,17 +1,12 @@
 class ProjectPhoto < ActiveRecord::Base
   belongs_to :project
-  attr_accessor :image_file_name
-  attr_accessor :image_content_type
-  attr_accessor :image_file_size
-  attr_accessor :image_updated_at
-
-  validates_attachment :image, :presence => true,
-    :content_type => { :content_type => ["image/jpeg", "image/png"] },
-    :size => { :in => 0..5.megabytes }
-
   has_attached_file :image,
     :storage => :s3,
     :s3_credentials => S3_CREDENTIALS,
     :path => "/project_photos/:style/:id/:filename",
     :styles => { :medium => "400px>" }
+
+  validates_attachment :image, :presence => true,
+    :content_type => { :content_type => ["image/jpeg", "image/png"] },
+    :size => { :in => 0..5.megabytes }
 end
