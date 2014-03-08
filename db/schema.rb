43741830aae9235cf75072b7b7936fbb60cd9c6c
Mark Miyashita <negativetwelve@gmail.com>
2014-03-08 11:01:27 -0800

    Add hack photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308064442) do
+ActiveRecord::Schema.define(version: 20140308183107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,12 @@ ActiveRecord::Schema.define(version: 20140308064442) do
     t.string   "location"
     t.date     "date"
     t.time     "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "hack_photos", force: true do |t|
+    t.integer  "hackathon_submission_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
