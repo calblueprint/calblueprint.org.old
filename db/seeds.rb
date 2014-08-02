4647a29a9c2103b6c010f91a56c4f0f635f250e3
Sam Lau <samlau95@gmail.com>
2014-08-02 14:52:17 -0700

    Add seed https://app.asana.com/0/15026852615861/15027068870944

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-Semester.create! year: '2014', semester: 'Spring', current: false
+s = Semester.create! year: '2014', semester: 'Fall', current: true
 Hackathon.create! semester: Semester.first, name: 'Hack 1',
   registration_link: 'google.com', starts: DateTime.now - 1.day,
   ends: DateTime.new + 1.day, location: 'Soda', description: 'Hello', public: true
+
+u = User.create! name: 'Admin', email: 'admin@gmail.com', password: 'password'
+Role.create! user: u, semester: s, name: 'VP of Technology', user_type: 'exec'
