11ef9417fac5041b785b8d35d638856f09158017
Sam Lau <samlau95@gmail.com>
2014-08-02 15:53:56 -0700

    Conditional linking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     text.gsub(" ", "-").downcase
   end
 
+  def link_unless(condition, url, options=nil, &block)
+    unless condition
+      content_tag :a, capture(&block), href: url, target: '_blank'
+    else
+      content_tag_string 'Hello', capture(&block), options
+    end
+  end
+
 end
