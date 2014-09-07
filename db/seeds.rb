5d65ec751d5e6526345b7ff6a7c4a442f82d404a
Mark Miyashita <negativetwelve@gmail.com>
2014-09-06 21:18:18 -0700

    Make the seedfile re-runnable

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-s = Semester.create! year: '2014', semester: 'Fall', current: true
-Hackathon.create! semester: Semester.first, name: 'Hack 1',
-  registration_link: 'google.com', starts: DateTime.now - 1.day,
-  ends: DateTime.new + 1.day, location: 'Soda', description: 'Hello', public: true
+s = Semester.where(year: '2014', semester: 'Fall', current: true).first_or_create
+Hackathon.where(
+  semester: Semester.first,
+  name: 'Hack 1',
+  registration_link: 'google.com',
+  starts: DateTime.now - 1.day,
+  ends: DateTime.new + 1.day,
+  location: 'Soda',
+  description: 'Hello',
+  public: true,
+).first_or_create
 
-u = User.create! name: 'Admin', email: 'admin@gmail.com', password: 'password'
-Role.create! user: u, semester: s, name: 'VP of Technology', user_type: 'exec'
+u = User.where(name: 'Admin', email: 'admin@gmail.com').first_or_create
+u.password = "password"
+u.save
+
+p = Position.where(name: "VP of Technology", user_type: "exec").first_or_create
+Role.where(user: u, semester: s, position: p, name: p.name, user_type: p.user_type).first_or_create
