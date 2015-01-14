76e0118130d3f1cca5cb8dd1bafab54f11f4e8ff
Sam Lau <samlau95@gmail.com>
2015-01-13 16:45:46 -0800

    Add faq with expander

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,3 +48,13 @@ $(function() {
     sortList: [[2,1], [3,1]]
   });
 });
+
+// Expander js
+$(document).ready(function() {
+  var expanderTrigger = document.getElementById("js-expander-trigger");
+  var expanderContent = document.getElementById("js-expander-content");
+
+  $('#js-expander-trigger').click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
